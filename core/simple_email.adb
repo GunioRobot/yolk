d7635cf7c7c903ca59962a1cfebea87a866c1edf
Thomas Løcke <thomas@12boo.net>
2010-11-05 15:45:59 +0100

    Adding file attachment support and more MIME types.
    The goal of the simple_email interface is to be _SIMPLE_. It must be very
    easy to send multipart/alternative or multipart/mixed emails.
    
    The new interface allows for adding recipients and attachments easily.
    
    HTML parts are favored over Text parts, unless the HTML part is empty.
    
    	modified:   core/simple_email.adb
    	modified:   core/simple_email.ads

--- core/simple_email.adb
+++ core/simple_email.adb
@@ -21,58 +21,146 @@
 --                                                                           --
 -------------------------------------------------------------------------------
 
-with AWS.Attachments;
+with Ada.Directories; use Ada.Directories;
+with AWS.Headers;
 with AWS.MIME;
-with AWS.SMTP;
 with AWS.SMTP.Client;
 
 package body Simple_Email is
+
+   ----------------------
+   --  Add_Attachment  --
+   ----------------------
+
+   procedure Add_Attachment (ES                 : in out Email_Structure;
+                             Attach_File        : in     String;
+                             Encode_Attachment  : in     Encoding := Base64)
+   is
+
+      An_Attachment : File_Attachment;
+
+   begin
+
+      An_Attachment.File   := To_Unbounded_String (Attach_File);
+      if Encode_Attachment = Base64 then
+         An_Attachment.Encode := AWS.Attachments.Base64;
+      else
+         An_Attachment.Encode := AWS.Attachments.None;
+      end if;
+      ES.Attachments_List.Append (An_Attachment);
+
+   end Add_Attachment;
+
+   ---------------------
+   --  Add_Recipient  --
+   ---------------------
+
+   procedure Add_Recipient (ES         : in out Email_Structure;
+                            To_Address : in     String;
+                            To_Name    : in     String)
+   is
+   begin
+
+      ES.To_List.Append (AWS.SMTP.E_Mail (Name    => To_Name,
+                                          Address => To_Address));
+
+   end Add_Recipient;
+
+   --------------------
+   --  Create_Email  --
+   --------------------
+
+   procedure Create_Email (ES                : in out Email_Structure;
+                           HTML_Part         : in     String;
+                           Text_Part         : in     String;
+                           From_Address      : in     String;
+                           From_Name         : in     String;
+                           To_Address        : in     String;
+                           To_Name           : in     String;
+                           Subject           : in     String := "(no subject)";
+                           Charset           : in     String := "iso-8859-1";
+                           Attach_File       : in     String := "";
+                           Encode_Attachment : in     Encoding := Base64;
+                           SMTP_Server       : in     String := "localhost")
+   is
+   begin
+
+      ES.HTML_Part := To_Unbounded_String (HTML_Part);
+      ES.Text_Part := To_Unbounded_String (Text_Part);
+
+      ES.From := AWS.SMTP.E_Mail (Name    => From_Name,
+                                  Address => From_Address);
+
+      Add_Recipient (ES         => ES,
+                     To_Address => To_Address,
+                     To_Name    => To_Name);
+
+      ES.Subject := To_Unbounded_String (Subject);
+
+      ES.Charset := To_Unbounded_String (Charset);
+
+      Add_Attachment (ES                => ES,
+                      Attach_File       => Attach_File,
+                      Encode_Attachment => Encode_Attachment);
+
+      ES.SMTP_List.Append (To_Unbounded_String (SMTP_Server));
+
+   end Create_Email;
+
+   ---------------------
+   --  Get_Recipient  --
+   ---------------------
+
+   function Get_Recipients (ES : in Email_Structure) return AWS.SMTP.Recipients
+   is
+
+      Recipient_List : AWS.SMTP.Recipients (1 .. ES.To_List.Last_Index);
+
+   begin
+
+      --  Populate the Recipient_List array with the elements found in the
+      --  To_List vector.
+      if Recipient_List'Length > 1 then
+         for i in Recipient_List'Range loop
+            Recipient_List (i) := ES.To_List.Element (i);
+         end loop;
+      else
+         Recipient_List (1) := ES.To_List.Element (1);
+      end if;
+
+      return Recipient_List;
+
+   end Get_Recipients;
 
    ------------
    --  Send  --
    ------------
 
-   function Send (HTML_Part   : in String;
-                  Text_Part   : in String;
-                  From_Email  : in String;
-                  From_Name   : in String;
-                  To_Email    : in String;
-                  To_Name     : in String;
-                  Subject     : in String;
-                  Charset     : in String := "iso-8859-1";
-                  SMTP_Server : in String) return Boolean
+   function Send (ES : Email_Structure) return Boolean
    is
 
-      SMTP        : constant AWS.SMTP.Receiver
-        := AWS.SMTP.Client.Initialize (SMTP_Server);
-      E_Content   : AWS.Attachments.List;
-      Alter       : AWS.Attachments.Alternatives;
-      Status      : AWS.SMTP.Status;
+      SMTP              : constant AWS.SMTP.Receiver
+        := AWS.SMTP.Client.Initialize (TS (ES.SMTP_List.Element (1)));
+      Email_Contents    : AWS.Attachments.List;
+      Status            : AWS.SMTP.Status;
 
    begin
 
-      AWS.Attachments.Add
-        (Parts => Alter,
-         Data  => AWS.Attachments.Value
-           (Data => Text_Part,
-            Content_Type => AWS.MIME.Text_Plain & "; charset=" & Charset));
-      AWS.Attachments.Add
-        (Parts => Alter,
-         Data  => AWS.Attachments.Value
-           (Data => HTML_Part,
-            Content_Type => AWS.MIME.Text_HTML & "; charset=" & Charset));
+      --  Add the alternative parts.
+      Set_Alternative_Parts (C   => Email_Contents,
+                             ES  => ES);
 
-      AWS.Attachments.Add (Attachments => E_Content,
-                           Parts       => Alter);
+      --  Add the file attachments.
+      Set_File_Attachments (C  => Email_Contents,
+                            ES => ES);
 
+      --  Send the email.
       AWS.SMTP.Client.Send
         (Server      => SMTP,
-         From        => AWS.SMTP.E_Mail
-           (From_Name, From_Email),
-         To          => AWS.SMTP.Recipients'
-           (1 => AWS.SMTP.E_Mail (To_Name, To_Email)),
-         Subject     => Subject,
-         Attachments => E_Content,
+         From        => ES.From,
+         To          => Get_Recipients (ES),
+         Subject     => To_String (ES.Subject),
+         Attachments => Email_Contents,
          Status      => Status);
 
       if AWS.SMTP.Is_Ok (Status) then
@@ -83,33 +171,119 @@ package body Simple_Email is
 
    end Send;
 
+   ------------------------
+   --  Set_Alternatives  --
+   ------------------------
+
+   procedure Set_Alternative_Parts (C  : in out AWS.Attachments.List;
+                                    ES : in     Email_Structure)
+   is
+
+      Alternative_Parts : AWS.Attachments.Alternatives;
+
+   begin
+
+      --  Alternative parts.
+      --  These are added with the least favorable part first and the most
+      --  favorable part last.
+      --  Alternative parts are simply just different versions of the same
+      --  content, usually a text and a HTML version.
+      --  If there a HTML part, then we favor that. Only when the HTML part is
+      --  empty do we favor the Text part.
+      if ES.HTML_Part = "" then
+         AWS.Attachments.Add
+           (Parts => Alternative_Parts,
+            Data  => AWS.Attachments.Value
+              (Data         => TS (ES.HTML_Part),
+               Content_Type => AWS.MIME.Text_HTML &
+               "; charset=" & TS (ES.Charset)));
+
+         AWS.Attachments.Add
+           (Parts => Alternative_Parts,
+            Data  => AWS.Attachments.Value
+              (Data         => TS (ES.Text_Part),
+               Content_Type => AWS.MIME.Text_Plain &
+               "; charset=" & TS (ES.Charset)));
+      else
+         AWS.Attachments.Add
+           (Parts => Alternative_Parts,
+            Data  => AWS.Attachments.Value
+              (Data         => TS (ES.Text_Part),
+               Content_Type => AWS.MIME.Text_Plain &
+               "; charset=" & TS (ES.Charset)));
+
+         AWS.Attachments.Add
+           (Parts => Alternative_Parts,
+            Data  => AWS.Attachments.Value
+              (Data         => TS (ES.HTML_Part),
+               Content_Type => AWS.MIME.Text_HTML &
+               "; charset=" & TS (ES.Charset)));
+      end if;
+
+      AWS.Attachments.Add (Attachments => C,
+                           Parts       => Alternative_Parts);
+
+   end Set_Alternative_Parts;
+
+   ----------------------------
+   --  Set_File_Attachments  --
+   ----------------------------
+
+   procedure Set_File_Attachments (C   : in out AWS.Attachments.List;
+                                   ES  : in     Email_Structure)
+   is
+
+      ESA : File_Attachments.Vector renames ES.Attachments_List;
+
+   begin
+
+      for i in ESA.First_Index .. ESA.Last_Index loop
+         AWS.Attachments.Add
+           (Attachments => C,
+            Filename    => To_String (ESA.Element (i).File),
+            Headers     => AWS.Headers.Empty_List,
+            Name        => Simple_Name (To_String (ESA.Element (i).File)),
+            Encode      => ESA.Element (i).Encode);
+      end loop;
+--        AWS.Attachments.Add (Attachments => C,
+--                             Filename    => ES.Attachments_List,
+--                             Headers     => AWS.Headers.Empty_List,
+--                             Name        => "",
+--                             Encode      => AWS.Attachments.Base64);
+--
+--        AWS.Attachments.Add (Attachments => C,
+--                             Filename    => "test.lyx",
+--                             Headers     => AWS.Headers.Empty_List,
+--                             Name        => "",
+--                             Encode      => AWS.Attachments.Base64);
+
+   end Set_File_Attachments;
+
    ------------
    --  Send  --
    ------------
 
-   procedure Send (HTML_Part   : in String;
-                   Text_Part   : in String;
-                   From_Email  : in String;
-                   From_Name   : in String;
-                   To_Email    : in String;
-                   To_Name     : in String;
-                   Subject     : in String;
-                   Charset     : in String := "iso-8859-1";
-                   SMTP_Server : in String;
-                   Success     : in out Boolean)
-   is
-   begin
-
-      Success := Send (HTML_Part   => HTML_Part,
-                       Text_Part   => Text_Part,
-                       From_Email  => From_Email,
-                       From_Name   => From_Name,
-                       To_Email    => To_Email,
-                       To_Name     => To_Name,
-                       Subject     => Subject,
-                       Charset     => Charset,
-                       SMTP_Server => SMTP_Server);
-
-   end Send;
+--     procedure Send (Parts         : in Alternative_Parts;
+--                     From_Email    : in String;
+--                     From_Name     : in String;
+--                     To_Email      : in String;
+--                     To_Name       : in String;
+--                     Subject       : in String;
+--                     Charset       : in String := "iso-8859-1";
+--                     SMTP_Server   : in String;
+--                     Success       : in out Boolean)
+--     is
+--     begin
+--
+--        Success := Send (Parts        => Parts,
+--                         From_Email   => From_Email,
+--                         From_Name    => From_Name,
+--                         To_Email     => To_Email,
+--                         To_Name      => To_Name,
+--                         Subject      => Subject,
+--                         Charset      => Charset,
+--                         SMTP_Server  => SMTP_Server);
+--
+--     end Send;
 
 end Simple_Email;
--- core/simple_email.ads
+++ core/simple_email.ads
@@ -20,18 +20,40 @@
 --  MA 02110 - 1301, USA.                                                    --
 --                                                                           --
 -------------------------------------------------------------------------------
+with Ada.Containers.Vectors;  use Ada.Containers;
+with Ada.Strings.Unbounded;   use Ada.Strings.Unbounded;
+with AWS.Attachments;
+with AWS.SMTP;                use type AWS.SMTP.E_Mail_Data;
 
 package Simple_Email is
 
-   function Send (HTML_Part   : in String;
-                  Text_Part   : in String;
-                  From_Email  : in String;
-                  From_Name   : in String;
-                  To_Email    : in String;
-                  To_Name     : in String;
-                  Subject     : in String;
-                  Charset     : in String := "iso-8859-1";
-                  SMTP_Server : in String) return Boolean;
+   type Email_Structure is private;
+   type Encoding is (None, Base64);
+
+   function TS (US : Unbounded_String) return String renames To_String;
+
+   procedure Add_Attachment (ES                 : in out Email_Structure;
+                             Attach_File        : in     String;
+                             Encode_Attachment  : in     Encoding := Base64);
+
+   procedure Add_Recipient (ES         : in out Email_Structure;
+                            To_Address : in     String;
+                            To_Name    : in     String);
+
+   procedure Create_Email (ES                : in out Email_Structure;
+                           HTML_Part         : in     String;
+                           Text_Part         : in     String;
+                           From_Address      : in     String;
+                           From_Name         : in     String;
+                           To_Address        : in     String;
+                           To_Name           : in     String;
+                           Subject           : in     String := "(no subject)";
+                           Charset           : in     String := "iso-8859-1";
+                           Attach_File       : in     String := "";
+                           Encode_Attachment : in     Encoding := Base64;
+                           SMTP_Server       : in     String := "localhost");
+
+   function Send (ES : Email_Structure) return Boolean;
    --  Compose and send a multipart email messages. This is for simple messages
    --  only. Attachments are not supported. If you need such "fancy" things
    --  then AWS.SMTP is what you're looking for.
@@ -39,21 +61,52 @@ package Simple_Email is
    --    The Charset string should of course match the encoding used in the
    --    HTML_Part and Text_Part.
 
-   procedure Send (HTML_Part   : in String;
-                   Text_Part   : in String;
-                   From_Email  : in String;
-                   From_Name   : in String;
-                   To_Email    : in String;
-                   To_Name     : in String;
-                   Subject     : in String;
-                   Charset     : in String := "iso-8859-1";
-                   SMTP_Server : in String;
-                   Success     : in out Boolean);
+--     procedure Send (Parts         : in Alternative_Parts;
+--                     From_Email    : in String;
+--                     From_Name     : in String;
+--                     To_Email      : in String;
+--                     To_Name       : in String;
+--                     Subject       : in String;
+--                     Charset       : in String := "iso-8859-1";
+--                     SMTP_Server   : in String;
+--                     Success       : in out Boolean);
    --  Compose and send a multipart email messages. This is for simple messages
    --  only. Attachments are not supported. If you need such "fancy" things
    --  then AWS.SMTP is what you're looking for.
    --  NOTE:
    --    The Charset string should of course match the encoding used in the
    --    HTML_Part and Text_Part.
+
+private
+
+   type File_Attachment is record
+      File     : Unbounded_String;
+      Encode   : AWS.Attachments.Encoding;
+   end record;
+   package File_Attachments is new Vectors (Positive, File_Attachment);
+
+   package Recipients is new Vectors (Positive, AWS.SMTP.E_Mail_Data);
+
+   package SMTP_Servers is new Vectors (Positive, Unbounded_String);
+
+   type Email_Structure is record
+      HTML_Part         : Unbounded_String;
+      Text_Part         : Unbounded_String;
+      From              : AWS.SMTP.E_Mail_Data;
+      To_List           : Recipients.Vector;
+      Subject           : Unbounded_String;
+      Charset           : Unbounded_String;
+      Attachments_List  : File_Attachments.Vector;
+      SMTP_List         : SMTP_Servers.Vector;
+   end record;
+
+   function Get_Recipients (ES : in Email_Structure)
+                            return AWS.SMTP.Recipients;
+
+   procedure Set_Alternative_Parts (C  : in out AWS.Attachments.List;
+                                    ES : in     Email_Structure);
+
+   procedure Set_File_Attachments (C   : in out AWS.Attachments.List;
+                                   ES  : in     Email_Structure);
 
 end Simple_Email;
