c6c85025a81aa0079ec15cf12ab74779698157f5
Thomas Løcke <thomas@12boo.net>
2010-12-09 09:31:58 +0100

    Fixed a typo in the copyright header.
    	modified:   core/config_file_parser.adb
    	modified:   core/config_file_parser.ads
    	modified:   core/configuration.adb
    	modified:   core/configuration.ads
    	modified:   core/connect_to_db-postgresql.adb
    	modified:   core/connect_to_db-postgresql.ads
    	modified:   core/connect_to_db.adb
    	modified:   core/connect_to_db.ads
    	modified:   core/db_connection.adb
    	modified:   core/db_connection.ads
    	modified:   core/handlers.adb
    	modified:   core/handlers.ads
    	modified:   core/not_found.adb
    	modified:   core/not_found.ads
    	modified:   core/process_control.adb
    	modified:   core/process_control.ads
    	modified:   core/rotating_log.adb
    	modified:   core/rotating_log.ads
    	modified:   core/simple_email.adb
    	modified:   core/simple_email.ads
    	modified:   core/static_content.adb
    	modified:   core/static_content.ads
    	modified:   core/unknown_content.adb
    	modified:   core/unknown_content.ads
    	modified:   core/utilities.adb
    	modified:   core/utilities.ads
    	modified:   my_configuration/my_configuration.adb
    	modified:   my_configuration/my_configuration.ads
    	modified:   my_handlers/my_handlers.adb
    	modified:   my_handlers/my_handlers.ads
    	modified:   my_view/view.adb
    	modified:   my_view/view.ads
    	modified:   yolk.adb
    	modified:   yolk.gpr

--- core/process_control.ads
+++ core/process_control.ads
@@ -12,7 +12,7 @@
 --  Yolk is free software;  you can  redistribute it  and/or modify it under --
 --  terms of the  GNU General Public License as published  by the Free Soft- --
 --  ware  Foundation;  either version 2,  or (at your option) any later ver- --
---  sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+--  sion.  Yolk is distributed in the hope that it will be useful, but WITH- --
 --  OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
 --  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
 --  for  more details.  You should have  received  a copy of the GNU General --
