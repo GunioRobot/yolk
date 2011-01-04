db531a4c990630c102efad579f20fa8e2c10af42
Thomas Løcke <thomas@12boo.net>
2011-01-04 17:14:37 +0100

    Added a whole bunch of AWS configuration parameters to the Yolk config system
    	modified:   core/configuration.adb
    	modified:   core/configuration.ads
    	modified:   exe/configuration/config.ini

--- core/configuration.adb
+++ core/configuration.adb
@@ -36,18 +36,52 @@ package body Configuration is
 
    begin
 
-      AWS.Config.Set.Parameter
-        (Config        => Object,
-         Name          => "Admin_Password",
-         Value         => Config.Get (Admin_Password));
+      AWS.Config.Set.Accept_Queue_Size
+        (O     => Object,
+         Value => Config.Get (Accept_Queue_Size));
+
+      AWS.Config.Set.Admin_Password
+        (O     => Object,
+         Value => Config.Get (Admin_Password));
 
       AWS.Config.Set.Admin_URI
         (O     => Object,
          Value => Config.Get (Admin_URI));
 
+      AWS.Config.Set.Case_Sensitive_Parameters
+        (O     => Object,
+         Value => Config.Get (Case_Sensitive_Parameters));
+
       AWS.Config.Set.Certificate
         (O        => Object,
          Filename => Config.Get (Certificate));
+
+      AWS.Config.Set.Check_URL_Validity
+        (O     => Object,
+         Value => Config.Get (Check_URL_Validity));
+
+      AWS.Config.Set.Cleaner_Client_Data_Timeout
+        (O     => Object,
+         Value => Config.Get (Cleaner_Client_Data_Timeout));
+
+      AWS.Config.Set.Cleaner_Client_Header_Timeout
+        (O     => Object,
+         Value => Config.Get (Cleaner_Client_Header_Timeout));
+
+      AWS.Config.Set.Cleaner_Server_Response_Timeout
+        (O     => Object,
+         Value => Config.Get (Cleaner_Server_Response_Timeout));
+
+      AWS.Config.Set.Cleaner_Wait_For_Client_Timeout
+        (O     => Object,
+         Value => Config.Get (Cleaner_Wait_For_Client_Timeout));
+
+      AWS.Config.Set.Context_Lifetime
+        (Value => Config.Get (Context_Lifetime));
+
+      AWS.Config.Set.Directory_Browser_Page
+        (O     => Object,
+         Value => Config.Get (System_Templates_Path) & "/aws_directory.tmpl");
 
       AWS.Config.Set.Error_Log_Filename_Prefix
         (O     => Object,
@@ -57,6 +91,50 @@ package body Configuration is
         (O     => Object,
          Value => Config.Get (Error_Log_Split_Mode));
 
+      AWS.Config.Set.Exchange_Certificate
+        (O     => Object,
+         Value => Config.Get (Exchange_Certificate));
+
+      AWS.Config.Set.Force_Client_Data_Timeout
+        (O     => Object,
+         Value => Config.Get (Force_Client_Data_Timeout));
+
+      AWS.Config.Set.Force_Client_Header_Timeout
+        (O     => Object,
+         Value => Config.Get (Force_Client_Header_Timeout));
+
+      AWS.Config.Set.Force_Server_Response_Timeout
+        (O     => Object,
+         Value => Config.Get (Force_Server_Response_Timeout));
+
+      AWS.Config.Set.Force_Wait_For_Client_Timeout
+        (O     => Object,
+         Value => Config.Get (Force_Wait_For_Client_Timeout));
+
+      AWS.Config.Set.Free_Slots_Keep_Alive_Limit
+        (O     => Object,
+         Value => Config.Get (Free_Slots_Keep_Alive_Limit));
+
+      AWS.Config.Set.Hotplug_Port
+        (O     => Object,
+         Value => Config.Get (Hotplug_Port));
+
+      AWS.Config.Set.Keep_Alive_Force_Limit
+        (O     => Object,
+         Value => Config.Get (Keep_Alive_Force_Limit));
+
+      AWS.Config.Set.Key
+        (O        => Object,
+         Filename => Config.Get (Key));
+
+      AWS.Config.Set.Line_Stack_Size
+        (O     => Object,
+         Value => Config.Get (Line_Stack_Size));
+
+      AWS.Config.Set.Log_Extended_Fields
+        (O     => Object,
+         Value => Config.Get (Log_Extended_Fields));
+
       AWS.Config.Set.Log_File_Directory
         (O     => Object,
          Value => Config.Get (Log_File_Directory));
@@ -65,23 +143,28 @@ package body Configuration is
         (O     => Object,
          Value => Config.Get (Log_Filename_Prefix));
 
-      AWS.Config.Set.Parameter
-        (Config        => Object,
-         Name          => "Log_Size_Limit",
-         Value         => Config.Get (Log_Size_Limit));
+      AWS.Config.Set.Log_Size_Limit
+        (O     => Object,
+         Value => Config.Get (Log_Size_Limit));
 
       AWS.Config.Set.Log_Split_Mode
         (O     => Object,
          Value => Config.Get (Log_Split_Mode));
 
+      AWS.Config.Set.Max_Concurrent_Download
+        (Value => Config.Get (Max_Concurrent_Download));
+
       AWS.Config.Set.Max_Connection
         (O     => Object,
          Value => Config.Get (Max_Connection));
 
-      AWS.Config.Set.Parameter
-        (Config        => Object,
-         Name          => "MIME_Types",
-         Value         => Config.Get (MIME_Types));
+      AWS.Config.Set.MIME_Types
+        (O     => Object,
+         Value => Config.Get (MIME_Types));
+
+      AWS.Config.Set.Receive_Timeout
+        (O     => Object,
+         Value => Config.Get (Receive_Timeout));
 
       AWS.Config.Set.Reuse_Address
         (O     => Object,
@@ -95,6 +178,14 @@ package body Configuration is
         (O    => Object,
          Mode => Config.Get (Security_Mode));
 
+      AWS.Config.Set.Send_Timeout
+        (O     => Object,
+         Value => Config.Get (Send_Timeout));
+
+      AWS.Config.Set.Server_Host
+        (O     => Object,
+         Value => Config.Get (Server_Host));
+
       AWS.Config.Set.Server_Name
         (O     => Object,
          Value => Config.Get (Server_Name));
@@ -107,6 +198,9 @@ package body Configuration is
         (O     => Object,
          Value => Config.Get (Session));
 
+      AWS.Config.Set.Session_Cleanup_Interval
+        (Value => Config.Get (Session_Cleanup_Interval));
+
       AWS.Config.Set.Session_Lifetime
         (Value => Config.Get (Session_Lifetime));
 
@@ -117,6 +211,24 @@ package body Configuration is
       AWS.Config.Set.Status_Page
         (O     => Object,
          Value => Config.Get (Status_Page));
+
+      AWS.Config.Set.Transient_Cleanup_Interval
+        (Value => Config.Get (Transient_Cleanup_Interval));
+
+      AWS.Config.Set.Transient_Lifetime
+        (Value => Config.Get (Transient_Lifetime));
+
+      AWS.Config.Set.Upload_Directory
+        (O     => Object,
+         Value => Config.Get (Upload_Directory));
+
+      AWS.Config.Set.Upload_Size_Limit
+        (O     => Object,
+         Value => Config.Get (Upload_Size_Limit));
+
+      AWS.Config.Set.WWW_Root
+        (O     => Object,
+         Value => Config.Get (WWW_Root));
 
       return Object;
 
--- core/configuration.ads
+++ core/configuration.ads
@@ -31,17 +31,27 @@ with Utilities; use Utilities;
 
 package Configuration is
 
-   type Keys is (Admin_Password, -- AWS
-                 Admin_URI, -- AWS
+   type Keys is (Accept_Queue_Size, --  AWS
+                 Admin_Password, --  AWS
+                 Admin_URI, --  AWS
                  Amount_Of_Log_Files_To_Keep,
-                 Certificate, -- AWS
+                 Case_Sensitive_Parameters, --  AWS
+                 Certificate, --  AWS
+                 Check_URL_Validity, --  AWS
+                 Cleaner_Client_Data_Timeout,
+                 Cleaner_Client_Header_Timeout, --  AWS
+                 Cleaner_Server_Response_Timeout, --  AWS
+                 Cleaner_Wait_For_Client_Timeout, --  AWS
+                 Context_Lifetime, --  AWS
                  Error_Log_Filename_Prefix, -- AWS
-                 Error_Log_Split_Mode, -- AWS
+                 Error_Log_Split_Mode, --  AWS
+                 Exchange_Certificate, --  AWS
+                 Force_Client_Data_Timeout, --  AWS
+                 Force_Client_Header_Timeout, --  AWS
+                 Force_Server_Response_Timeout, --  AWS
+                 Force_Wait_For_Client_Timeout, --  AWS
+                 Free_Slots_Keep_Alive_Limit, --  AWS
                  GNATCOLL_Traces_Ini_File,
-                 Log_File_Cleanup_Interval,
-                 Log_Filename_Prefix, -- AWS
-                 Log_Size_Limit, -- AWS
-                 Log_Split_Mode, -- AWS
                  Handler_CSS,
                  Handler_GIF,
                  Handler_HTML,
@@ -52,24 +62,42 @@ package Configuration is
                  Handler_XML,
                  Handler_XSL,
                  Handler_Unknown,
+                 Hotplug_Port, --  AWS
                  Immediate_Flush,
-                 Log_File_Directory, -- AWS
-                 Max_Connection, -- AWS
+                 Keep_Alive_Force_Limit, --  AWS
+                 Key, --  AWS
+                 Line_Stack_Size, --  AWS
+                 Log_Extended_Fields, --  AWS
+                 Log_File_Cleanup_Interval,
+                 Log_File_Directory, --  AWS
+                 Log_Filename_Prefix, --  AWS
+                 Log_Size_Limit, --  AWS
+                 Log_Split_Mode, --  AWS
+                 Max_Concurrent_Download, --  AWS
+                 Max_Connection, --  AWS
                  Max_Slot_Count,
-                 MIME_Types, -- AWS
-                 Reuse_Address, -- AWS
+                 MIME_Types, --  AWS
+                 Receive_Timeout, --  AWS
+                 Reuse_Address, --  AWS
                  Rotating_Log_Size_Limit,
-                 Security, -- AWS
-                 Security_Mode, -- AWS
-                 Server_Name, -- AWS
-                 Server_Port, -- AWS
-                 Session, -- AWS
-                 Session_Data_File, -- AWS
-                 Session_Lifetime, -- AWS
-                 Session_Name, -- AWS
-                 Static_Content_Path,
-                 Status_Page, -- AWS
-                 System_Templates_Path);
+                 Security, --  AWS
+                 Security_Mode, --  AWS
+                 Send_Timeout, --  AWS
+                 Server_Host, --  AWS
+                 Server_Name, --  AWS
+                 Server_Port, --  AWS
+                 Session, --  AWS
+                 Session_Cleanup_Interval, --  AWS
+                 Session_Data_File,
+                 Session_Lifetime, --  AWS
+                 Session_Name, --  AWS
+                 Status_Page, --  AWS
+                 System_Templates_Path,
+                 Transient_Cleanup_Interval, --  AWS
+                 Transient_Lifetime, --  AWS
+                 Upload_Directory, --  AWS
+                 Upload_Size_Limit, --  AWS
+                 WWW_Root); --  AWS
    --  The valid configuration keys.
    --  These configuration keys are essential for core parts of Yolk, so you
    --  should not change them, unless you know what you're doing.
@@ -78,28 +106,48 @@ package Configuration is
      Ada.Strings.Unbounded.Unbounded_String;
 
    Default_Values : constant Defaults_Array :=
-                      (Admin_Password
+                      (Accept_Queue_Size
+                       => TUS ("64"),
+                       Admin_Password
                        => TUS ("0ac9c9d0c0b1ee058b65ae70c9aeb3a7"),
                        Admin_URI
                        => TUS ("/status"),
                        Amount_Of_Log_Files_To_Keep
                        => TUS ("30"),
+                       Case_Sensitive_Parameters
+                       => TUS ("True"),
                        Certificate
                        => TUS ("certificates/aws.pem"),
+                       Check_URL_Validity
+                       => TUS ("True"),
+                       Cleaner_Client_Data_Timeout
+                       => TUS ("28800.0"),
+                       Cleaner_Client_Header_Timeout
+                       => TUS ("7.0"),
+                       Cleaner_Server_Response_Timeout
+                       => TUS ("28800.0"),
+                       Cleaner_Wait_For_Client_Timeout
+                       => TUS ("80.0"),
+                       Context_Lifetime
+                       => TUS ("28800.0"),
                        Error_Log_Filename_Prefix
                        => TUS ("yolk_error"),
                        Error_Log_Split_Mode
                        => TUS ("Daily"),
+                       Exchange_Certificate
+                       => TUS ("False"),
+                       Force_Client_Data_Timeout
+                       => TUS ("10800.0"),
+                       Force_Client_Header_Timeout
+                       => TUS ("2.0"),
+                       Force_Server_Response_Timeout
+                       => TUS ("10800.0"),
+                       Force_Wait_For_Client_Timeout
+                       => TUS ("2.0"),
+                       Free_Slots_Keep_Alive_Limit
+                       => TUS ("1"),
                        GNATCOLL_Traces_Ini_File
                        => TUS ("configuration/GNATCOLL.SQL.Logs.ini"),
-                       Log_File_Cleanup_Interval
-                       => TUS ("3600"),
-                       Log_Filename_Prefix
-                       => TUS ("yolk_access"),
-                       Log_Size_Limit
-                       => TUS ("10_000_000"),
-                       Log_Split_Mode
-                       => TUS ("Daily"),
                        Handler_CSS
                        => TUS (".*\.css"),
                        Handler_GIF
@@ -120,16 +168,38 @@ package Configuration is
                        => TUS (".*\.xsl"),
                        Handler_Unknown
                        => TUS ("[^status].*"),
+                       Hotplug_Port
+                       => TUS ("8888"),
                        Immediate_Flush
                        => TUS ("False"),
+                       Keep_Alive_Force_Limit
+                       => TUS ("0"),
+                       Key
+                       => TUS (""),
+                       Line_Stack_Size
+                       => TUS ("16#150_000#"),
+                       Log_Extended_Fields
+                       => TUS (""),
+                       Log_File_Cleanup_Interval
+                       => TUS ("3600.0"),
                        Log_File_Directory
                        => TUS ("logs/"),
+                       Log_Filename_Prefix
+                       => TUS ("yolk_access"),
+                       Log_Size_Limit
+                       => TUS ("10_000_000"),
+                       Log_Split_Mode
+                       => TUS ("Daily"),
+                       Max_Concurrent_Download
+                       => TUS ("25"),
                        Max_Connection
                        => TUS ("5"),
                        Max_Slot_Count
                        => TUS ("3"),
                        MIME_Types
                        => TUS ("configuration/aws.mime"),
+                       Receive_Timeout
+                       => TUS ("30.0"),
                        Reuse_Address
                        => TUS ("False"),
                        Rotating_Log_Size_Limit
@@ -138,24 +208,38 @@ package Configuration is
                        => TUS ("False"),
                        Security_Mode
                        => TUS ("SSLv23"),
+                       Send_Timeout
+                       => TUS ("40.0"),
+                       Server_Host
+                       => TUS (""),
                        Server_Name
                        => TUS ("Yolk"),
                        Server_Port
                        => TUS ("4242"),
                        Session
                        => TUS ("False"),
+                       Session_Cleanup_Interval
+                       => TUS ("300.0"),
                        Session_Data_File
                        => TUS ("session/session.data"),
                        Session_Lifetime
                        => TUS ("1200"),
                        Session_Name
                        => TUS ("Yolk"),
-                       Static_Content_Path
-                       => TUS ("static_content"),
                        Status_Page
                        => TUS ("status/aws_status.thtml"),
                        System_Templates_Path
-                       => TUS ("templates/system"));
+                       => TUS ("templates/system"),
+                       Transient_Cleanup_Interval
+                       => TUS ("180.0"),
+                       Transient_Lifetime
+                       => TUS ("300.0"),
+                       Upload_Directory
+                       => TUS ("uploads"),
+                       Upload_Size_Limit
+                       => TUS ("16#500_000#"),
+                       WWW_Root
+                      => TUS ("static_content"));
    --  Default values for the configuration Keys. These values can be over-
    --  written by the configuration file given when instantiating the
    --  Config_File_Parser generic.
