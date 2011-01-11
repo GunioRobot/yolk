fecc13dd87d8866017bb0af5f4573b2c237ec725
Thomas Løcke <thomas@12boo.net>
2011-01-12 00:10:02 +0100

    Got rid of STDOUT output from Simple_Email and the valid XHTML PNG.
    As we're now using HTML5, the valid PNG is no longer necessary.
    
    	modified:   core/simple_email.adb
    	deleted:    exe/static_content/images/valid-xhtml10-blue.png

--- core/simple_email.adb
+++ core/simple_email.adb
@@ -23,7 +23,7 @@
 
 with Ada.Directories;
 with Utilities;
-with Ada.Text_IO;
+--  with Ada.Text_IO;
 --  with AWS.Headers;
 --  with AWS.MIME;
 --  with AWS.Utils;
@@ -181,7 +181,7 @@ package body Simple_Email is
      (ES : in out Email_Structure)
    is
 
-      use Ada.Text_IO;
+      --  use Ada.Text_IO;
 
    begin
 
@@ -189,13 +189,17 @@ package body Simple_Email is
 
       case ES.Type_Of_Email is
          when Text =>
-            Put_Line ("Text type");
+            null;
+            --  Put_Line ("Text type");
          when Text_With_Attachment =>
-            Put_Line ("Text_With_Attachment type");
+            null;
+            --  Put_Line ("Text_With_Attachment type");
          when Text_And_HTML =>
-            Put_Line ("Text_And_HTML type");
+            null;
+            --  Put_Line ("Text_And_HTML type");
          when Text_And_HTML_With_Attachment =>
-            Put_Line ("Text_And_HTML_With_Attachment type");
+            null;
+            --  Put_Line ("Text_And_HTML_With_Attachment type");
       end case;
 
    end Send;
