ba8fc60fff2bdfe1d0d2f632aa074b9cf11648ce
Thomas Løcke <thomas@12boo.net>
2011-02-02 11:09:55 +0100

    Got rid of the test database code from the view files.
    	modified:   ../my_view/view-index.adb
    	modified:   ../my_view/view.ads

--- my_view/view-index.adb
+++ my_view/view-index.adb
@@ -26,18 +26,11 @@ with Rotating_Log;
 with Email;
 --  with GNATCOLL.Email;
 --  with GNATCOLL.Email.Utils;
-with GNATCOLL.SQL;
-with GNATCOLL.SQL.Exec;
-
 --  with GNATCOLL.VFS; use GNATCOLL.VFS;
-
-with Ada.Text_IO;
 --  with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 --  with Ada.Calendar;
-
 --  with AWS.MIME;
 --  with AWS.Utils;
-
 --  with AWS.SMTP.Client;
 
 package body View.Index is
@@ -51,9 +44,7 @@ package body View.Index is
       return AWS.Response.Data
    is
 
-      use Ada.Text_IO;
       use AWS.Templates;
-      use GNATCOLL;
       use Email;
       use Rotating_Log;
 
@@ -61,29 +52,7 @@ package body View.Index is
 
       T : Translate_Set;
 
-      DB1 : constant SQL.Exec.Database_Connection := View.DB_One.Connection;
-      DB2 : constant SQL.Exec.Database_Connection := View.DB_Two.Connection;
-
-      C1 : SQL.Exec.Forward_Cursor;
-      C2 : SQL.Exec.Forward_Cursor;
-
    begin
-
-      C1.Fetch (Connection => DB1,
-                Query      => "select * from groups");
-      while C1.Has_Row loop
-         Put_Line (C1.Value (Field => 1));
-         C1.Next;
-      end loop;
-      DB1.Commit_Or_Rollback;
-
-      C2.Fetch (Connection => DB2,
-                Query      => "select * from status");
-      while C2.Has_Row loop
-         Put_Line (C2.Value (Field => 1));
-         C2.Next;
-      end loop;
-      DB2.Commit_Or_Rollback;
 
       Track (Handle     => Info,
              Log_String => "Testing the INFO track");
@@ -98,193 +67,63 @@ package body View.Index is
 
       declare
 
-         An_Email : Email_Structure;
-         Bn_Email : Email_Structure;
-         Cn_Email : Email_Structure;
+         Email : Email_Structure;
+         --  Bn_Email : Email_Structure;
+         --  Cn_Email : Email_Structure;
 
       begin
 
          --  Manually build and send an email.
-         Add_From (ES       => An_Email,
+         Add_From (ES       => Email,
                    Address  => "thomas@responsum.dk",
                    Name     => "Thomas Løcke",
-                   Charset  => UTF8);
+                   Charset  => ISO_8859_1);
 
-         Add_Recipient (ES       => An_Email,
+         Add_Recipient (ES       => Email,
                         Address  => "thomas.granvej6@gmail.com",
                         Name     => "Thomas Løcke",
-                        Charset  => UTF8);
+                        Charset  => ISO_8859_1);
 
-         Add_File_Attachment (ES           => An_Email,
-                              Path_To_File => "../../test.txt",
-                              Charset      => UTF8);
-
-         Add_SMTP_Server (ES     => An_Email,
+         Add_SMTP_Server (ES     => Email,
                           Host   => My.Config.Get (My.SMTP));
 
-         Set_Text_Part (ES       => An_Email,
-                        Part     => "Text ÆØÅ æøå",
-                        Charset  => UTF8);
+         Set_Text_Part (ES       => Email,
+                        Part     => "Text with attachment ÆØÅ æøå",
+                        Charset  => ISO_8859_1);
+         Set_HTML_Part (ES      => Email,
+                        Part    => "HTML <b>Test</b> ÆØÅ æøå",
+                        Charset => ISO_8859_1);
 
-         Set_HTML_Part (ES       => An_Email,
-                        Part     => "<b>HTML</b> ÆØÅ æøå",
-                        Charset  => UTF8);
+         Set_Subject (ES      => Email,
+                      Subject => "Some Subject with ÆØÅ and æøå",
+                      Charset => ISO_8859_1);
 
-         Send (ES => An_Email);
+         Send (ES => Email);
 
          --  Use a convenience procedure to build and send an email.
-         Add_Recipient (ES      => Bn_Email,
-                        Address => "tl@ada-dk.org",
-                        Charset => ISO_8859_1);
-         Add_Recipient (ES      => Bn_Email,
-                        Name    => "CCThomas Løcke",
-                        Address => "CCtl@ada-dk.org",
-                        Kind    => Email.Cc,
-                        Charset => ISO_8859_1);
-         Add_Recipient (ES      => Bn_Email,
-                        Name    => "BCCThomas Løcke",
-                        Address => "BCCtl@ada-dk.org",
-                        Kind    => Bcc,
-                        Charset => ISO_8859_1);
-         Send (ES             => Bn_Email,
-               From_Address   => "thomas@responsum.dk",
-               From_Name      => "Thomas Løcke",
-               To_Address     => "thomas@12boo.net",
-               To_Name        => "Thomas Løcke",
-               Subject        => "Text Type Test ÆØÅ æøå",
-               Text_Part      => "Text Type Test ÆØÅ ÆØÅ",
-               SMTP_Server    => "freja.serverbox.dk",
-               Charset        => ISO_8859_1);
+         --  Send (ES             => Bn_Email,
+         --        From_Address   => "thomas@responsum.dk",
+         --        From_Name      => "Thomas Løcke",
+         --        To_Address     => "thomas@12boo.net",
+         --        To_Name        => "Thomas Løcke",
+         --        Subject        => "Text Type Test ÆØÅ æøå",
+         --        Text_Part      => "Text Type Test ÆØÅ ÆØÅ",
+         --        SMTP_Server    => "freja.serverbox.dk",
+         --        Charset        => ISO_8859_1);
 
          --  Use a convenience procedure to build and send an email.
-         Send (ES             => Cn_Email,
-               From_Address   => "thomas@responsum.dk",
-               From_Name      => "Thomas Løcke",
-               To_Address     => "thomas@12boo.net",
-               To_Name        => "Thomas Løcke",
-               Subject        => "Test ÆØÅ æøå",
-               Text_Part      => "Test ÆØÅ ÆØÅ",
-               HTML_Part      => "<b>Test</b> ÆØÅ æøå",
-               SMTP_Server    => "freja.serverbox.dk",
-               Charset        => ISO_8859_1);
+         --  Send (ES             => Cn_Email,
+         --        From_Address   => "thomas@responsum.dk",
+         --        From_Name      => "Thomas Løcke",
+         --        To_Address     => "thomas@12boo.net",
+         --        To_Name        => "Thomas Løcke",
+         --        Subject        => "Test ÆØÅ æøå",
+         --        Text_Part      => "Test ÆØÅ ÆØÅ",
+         --        HTML_Part      => "<b>Test</b> ÆØÅ æøå",
+         --        SMTP_Server    => "freja.serverbox.dk",
+         --        Charset        => ISO_8859_1);
 
       end;
-
-      ------------------------
-      --  Text_Plain_Email  --
-      ------------------------
-
-      ---------------------
-      --  Text_With_Attachment  --
-      ----------------------------
---    Text_With_Attachment :
---    declare
---
---       A_File   : constant GNATCOLL.VFS.Virtual_File :=
---                    Locate_On_Path ("/home/thomas/test.txt");
---       Date     : Header;
---       From     : Header;
---       To       : Header;
---       MIME     : Header;
---       Subject  : Header;
---       CT       : Header;
---       CTE      : Header;
---       An_Email : Message := New_Message (Multipart_Mixed);
---       T_Payload : Message := New_Message (Text_Plain);
---       US       : Unbounded_String := Null_Unbounded_String;
---
---    begin
---
---       --  Set multipart boundaries
---       Set_Boundary (Msg      => An_Email,
---                     Boundary => AWS.Utils.Random_String (16));
---
---       New_Line;
---       Put_Line ("--> Text_With_Attachment Start <--");
---
---       --  First we set the text payload.
---       Set_Text_Payload (Msg       => T_Payload,
---                         Payload   => "Test ÆØÅ æøå",
---                         Charset   => Charset_ISO_8859_1);
---
---       --  Delete all headers for T_Payload
---       Delete_Headers (Msg  => T_Payload,
---                       Name => "");
---
---       --  Add Content-Type and Content-Transfer-Encoding headers to
---       --  T_Payload.
---       CT := Create (Name    => Content_Type,
---                     Value   => Text_Plain);
---       Set_Param (H           => CT,
---                  Param_Name  => "charset",
---                  Param_Value => Charset_ISO_8859_1);
---       Add_Header (Msg => T_Payload,
---                   H   => CT);
---
---       CTE := Create (Name    => Content_Transfer_Encoding,
---                      Value   => "8bit");
---       Add_Header (Msg => T_Payload,
---                   H   => CTE);
---
---       --  Add T_Payload to the An_Email message.
---       Add_Payload (Msg     => An_Email,
---                    Payload => T_Payload,
---                    First   => True);
---
---       --  Then we add an attachment
---       Attach (Msg                  => An_Email,
---               Path                 => A_File,
---               MIME_Type            => AWS.MIME.Content_Type ("test.txt"),
---               Description          => "A description of the file",
---               Charset              => Charset_ISO_8859_1);
---
---       --  Set the MIME preamble
---       Set_Preamble
---         (Msg      => An_Email,
---          Preamble => "This is a multi-part message in MIME format.");
---
---       --  Adding the headers to An_Email
---       Date := Create (Name => "Date",
---                       Value => Format_Date (Date => Ada.Calendar.Clock));
---       Add_Header (Msg => An_Email,
---                   H   => Date);
---
---       From := Create (Name    => "From",
---                       Value   => "Thomas Løcke",
---                       Charset => Charset_ISO_8859_1);
---       Append (H       => From,
---               Value   => " <thomas@responsum.dk>");
---       Add_Header (Msg => An_Email,
---                   H   => From);
---
---       To := Create (Name    => "To",
---                     Value   => "Thomas Løcke",
---                     Charset => Charset_ISO_8859_1);
---       Append (H       => To,
---               Value   => " <tl@ada-dk-org>");
---       Add_Header (Msg => An_Email,
---                   H   => To);
---
---       MIME := Create (Name    => MIME_Version,
---                       Value   => "1.0");
---       Add_Header (Msg => An_Email,
---                   H   => MIME);
---
---       Subject := Create (Name    => "Subject",
---                          Value   => "Text with attachment ÆØÅ æøå",
---                          Charset => Charset_ISO_8859_1);
---       Add_Header (Msg => An_Email,
---                   H   => Subject);
---
---       --  Finally we output the raw email.
---       To_String (Msg    => An_Email,
---                  Result => US);
---
---       Put_Line (To_String (US));
---
---       Put_Line ("--> Text_With_Attachment_End <--");
---
---    end Text_With_Attachment;
 
       ---------------------------------------
       --  Text_HTML_Multipart_Alternative  --
