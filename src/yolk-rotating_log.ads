6348b84882feb05ac8d746cfcaaebdb0aca5f687
Thomas Løcke <thomas@12boo.net>
2011-07-12 10:44:19 +0200

    More comments cleanups.

--- src/yolk-rotating_log.ads
+++ src/yolk-rotating_log.ads
@@ -27,11 +27,11 @@
 --  By adding new components to the Trace_Handles type you can create extra
 --  rotating log files.
 --  All rotating log file share the same configuration settings, which are
---  set in configuration/config.ini (path is relative to your application).
---  The Rotating_Log package make use of the GNATCOLL.Traces package for the
---  GNATCOLL_SQL trace handle. If you don't use any of the GNATCOLL database
---  stuff (see DB_Connection) in your application, you can disable that
---  specific trace in the configuration/GNATCOLL.SQL.Logs.Ini file.
+--  set in the Yolk.Configuration package.
+--  The Yolk.Rotating_Log package make use of the GNATCOLL.Traces package for
+--  the GNATCOLL_SQL trace handle. If you don't use any of the GNATCOLL
+--  database stuff (see DB_Connection) in your application, you can disable
+--  that specific trace in the GNATCOLL.SQL.Logs.Ini file.
 
 package Yolk.Rotating_Log is
 
@@ -42,10 +42,9 @@ package Yolk.Rotating_Log is
    --  web-application without logging enabled.
 
    type Trace_Handles is (Error, Info, GNATCOLL_SQL);
-   --  This is the core of the entire Rotating_Log package. It is here we
-   --  define the "traces" the system will accept. For each value defined in
-   --  this type, a corresponding log file is created, which we can add data to
-   --  by calling the Track procedure.
+   --  Here we define the "traces" the system will accept. For each value
+   --  defined in this type, a corresponding log file is created, which we can
+   --  add data to by calling the Track procedure.
    --
    --  IMPORTANT:
    --    The Error and Info handles should _not_ be removed, as they are used
@@ -54,7 +53,7 @@ package Yolk.Rotating_Log is
    --  NOTE:
    --    The GNATCOLL_SQL value is special. This handle is locked to the
    --    GNATCOLL.SQL traces, so if you want to access the output from
-   --    the SQL, SQL.SELECT and SQL.ERROR traces, then you must have this
+   --    the SQL, SQL.SELECT and SQL.ERROR traces, then you _must_ have this
    --    value defined and a valid GNATCOLL_Traces_Ini_File value.
    --    See the demo/exe/configuration/GNATCOLL.SQL.Logs.ini file for more
    --    information.
