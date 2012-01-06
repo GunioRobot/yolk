6c91bff36436a38a99cd40eba42636a81f0553e1
Thomas Løcke <thomas@12boo.net>
2012-01-06 23:32:59 +0100

    Updated to GPLv3.

--- src/yolk-config_file_parser.ads
+++ src/yolk-config_file_parser.ads
@@ -6,18 +6,23 @@
 --                                                                           --
 --                                  SPEC                                     --
 --                                                                           --
---                   Copyright (C) 2010-2011, Thomas Løcke                   --
+--                   Copyright (C) 2010-2012, Thomas Løcke                   --
 --                                                                           --
---  Yolk is free software;  you can  redistribute it  and/or modify it under --
---  terms of the  GNU General Public License as published  by the Free Soft- --
---  ware  Foundation;  either version 2,  or (at your option) any later ver- --
---  sion.  Yolk is distributed in the hope that it will be useful, but WITH- --
---  OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
---  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
---  for  more details.  You should have  received  a copy of the GNU General --
---  Public License  distributed with Yolk.  If not, write  to  the  Free     --
---  Software Foundation,  51  Franklin  Street,  Fifth  Floor, Boston,       --
---  MA 02110 - 1301, USA.                                                    --
+--  This library is free software;  you can redistribute it and/or modify    --
+--  it under terms of the  GNU General Public License  as published by the   --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any  --
+--  later version. This library is distributed in the hope that it will be   --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     --
+--                                                                           --
+--  As a special exception under Section 7 of GPL version 3, you are         --
+--  granted additional permissions described in the GCC Runtime Library      --
+--  Exception, version 3.1, as published by the Free Software Foundation.    --
+--                                                                           --
+--  You should have received a copy of the GNU General Public License and    --
+--  a copy of the GCC Runtime Library Exception along with this program;     --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+--  <http://www.gnu.org/licenses/>.                                          --
 --                                                                           --
 -------------------------------------------------------------------------------
 
